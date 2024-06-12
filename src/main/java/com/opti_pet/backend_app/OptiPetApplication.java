a3b2c06ada258d5b88731425a82e0cc9abfa7022
Dimo Penev <dimo.penev.dimov@strypes.eu>
2024-06-12 10:00:49 +0300

    Initial Commit

---
+++ src/main/java/com/opti_pet/backend_app/OptiPetApplication.java
@@ -0,0 +1,13 @@
+package com.opti_pet.backend_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptiPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptiPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opti_pet/backend_app/OptiPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opti_pet.backend_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptiPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
