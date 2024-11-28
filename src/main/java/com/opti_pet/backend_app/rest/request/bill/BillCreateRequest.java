9a4f5eec1148ad5df5d5fd69979537dcd140e11c
Dimo Dimov <dimo.penev.dimov@ict.eu>
2024-11-28 18:11:33 +0200

    add logic

--- src/main/java/com/opti_pet/backend_app/rest/request/bill/BillCreateRequest.java
+++ src/main/java/com/opti_pet/backend_app/rest/request/bill/BillCreateRequest.java
@@ -1,14 +1,9 @@
 package com.opti_pet.backend_app.rest.request.bill;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Builder;
 import org.hibernate.validator.constraints.UUID;
 
-import java.math.BigDecimal;
-
 @Builder
-public record BillCreateRequest(@UUID String patientId, Long discountId, @PositiveOrZero BigDecimal paidAmount,
-                                @NotNull Boolean hasInvoice, String note,
+public record BillCreateRequest(@UUID String patientId, Long discountId, Boolean hasInvoice, String note,
                                 String billTemplateId) {
 }
